3d7cda699d6264f52a487cbe61b2833c22158591
top1million <aasahashish@gmail.com>
2021-06-09 11:32:20 +0300

    amro

---
+++ java fundementals/javaFun/FizzBuzzTest.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+       FizzBuzz x=new FizzBuzz();
+       String c=x.fizzBuzz(91);
+       System.out.println(c);
+
+    }
+}
