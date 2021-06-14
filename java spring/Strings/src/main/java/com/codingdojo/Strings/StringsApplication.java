a9e4ee1a7da21b1cd91335d29a434d8a28f3de06
top1million <aasahashish@gmail.com>
2021-06-14 16:45:13 +0300

    strings

---
+++ java spring/Strings/src/main/java/com/codingdojo/Strings/StringsApplication.java
@@ -0,0 +1,27 @@
+package com.codingdojo.Strings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+	@RequestMapping("/")
+    // 3. Method that maps to the request route above
+    public String hello() { // 3
+            return "Hello client how are u doing!";
+    }
+    
+    @RequestMapping("/random")
+    public String random() {
+    	return "hello this is random from bootstrap";
+    }
+
+}
+
