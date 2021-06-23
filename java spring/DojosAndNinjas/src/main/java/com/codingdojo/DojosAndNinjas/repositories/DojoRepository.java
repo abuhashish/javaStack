c5e9b09f7cc3f5425ae5fac35c87175b2e7f85e5
top1million <aasahashish@gmail.com>
2021-06-23 12:56:09 +0300

    sql native in java

--- java spring/DojosAndNinjas/src/main/java/com/codingdojo/DojosAndNinjas/repositories/DojoRepository.java
+++ java spring/DojosAndNinjas/src/main/java/com/codingdojo/DojosAndNinjas/repositories/DojoRepository.java
@@ -3,6 +3,7 @@ package com.codingdojo.DojosAndNinjas.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +14,10 @@ import com.codingdojo.DojosAndNinjas.models.Dojo;
 public interface DojoRepository extends CrudRepository<Dojo,Long> {
 	List<Dojo> findAll();
 	Optional<Dojo> findById(Long id);
+	@Query("SELECT d FROM Dojo d JOIN d.ninjas n")
+    List<Dojo> joinDojosAndNinjas();
+    
+    // inner join retrieving dojos and ninjas
+    @Query("SELECT d, n FROM Dojo d JOIN d.ninjas n")
+    List<Object[]> joinDojosAndNinjas2();
 }
